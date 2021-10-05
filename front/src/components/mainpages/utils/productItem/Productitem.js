11fc28cf663201664b2bb522e35be08c60f58a10
alameensodiq <alameensodiq@yahoo.com>
2021-10-05 15:29:48 +0100

    commerce

---
+++ front/src/components/mainpages/utils/productItem/Productitem.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import BtnRender from './BtnRender'
+function Productitem({product,isAdmin}) {
+    return (
+        <div className="product_card">
+            {
+                isAdmin && <input type="checkbox" checked={product.checked}/>
+            }
+            <img src={product.images.url}  alt=""/>
+
+            <div className="product_box">
+                <h2 title={product.title}>{product.title}</h2>
+                <span>${product.price}</span>
+                <p>{product.description}</p>
+            </div>
+
+         <BtnRender product={product} />
+        </div>
+    )
+}
+
+export default Productitem
