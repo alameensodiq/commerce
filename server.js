8dfad15020e41b651e40449a758c7d46ba19253a
alameensodiq <alameensodiq@yahoo.com>
2021-10-06 13:15:50 +0100

    final deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
 
 const app= express()
@@ -40,7 +41,16 @@ app.get('/', (req, res)=> {
     })
 })
 
-
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'/front/build')));
+    app.get('*', (req, res)=> {
+        res.sendFile(path.join(__dirname, 'front', 'build', 'index.html'));
+    })
+} else {
+    app.get('/', (req, res)=> {
+        res.send('welcome on board please subscribe to our channel.Thanks')
+    })
+}
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> {
     console.log('Server is running on port', PORT)
